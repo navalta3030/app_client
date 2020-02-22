4998d0ec1f5ddd1a0134971b67131f52fba8bc0e
Mark Christian Navalta <navalta3030@gmail.com>
2020-02-21 21:36:14 -0800

    adding documentation

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,16 @@ import HomeIndex from "views/home/vh_index.js"
 import Test from "views/home/vh_test.js"
 let routes = [
   {
+    layout: "/home",
     path: "/index",
     name: "Home",
-    view: HomeIndex,
-    layout: "/home"
+    view: HomeIndex
   },
   {
+    layout: "/home",
     path: "/test",
     name: "Test",
-    view: Test,
-    layout: "/home"
+    view: Test
   }
 ];
 
