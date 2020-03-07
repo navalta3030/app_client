b8fbd287fbab038971ee6052169672d551824e35
navalta3030 <navalta3030@gmail.com>
2020-03-07 13:33:05 -0800

    added redux

--- src/index.js
+++ src/index.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
+import { Reducer } from "./reducers";
 import HomeLayout from 'layout/home_layout.js';
-
 
 import * as serviceWorker from 'serviceWorker.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'assets/scss/index.scss'
 
-
+const store = createStore(
+  combineReducers({
+      app: Reducer
+  }),
+  applyMiddleware(thunk) // ajax handling
+);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/home" render={props => <HomeLayout {...props} />} />
+  <Provider store={Provider}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/home" render={props => <HomeLayout {...props} />} />
 
-      <Redirect from="/" to="/home" />
-    </Switch>
-  </BrowserRouter>,
+        <Redirect from="/" to="/home" />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 
